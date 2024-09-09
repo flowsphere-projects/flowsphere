7b995d56676c85eb290037eadd975ea8d2e051c7
ZShUn <616394599@qq.com>
2024-09-09 21:45:17 +0800

    update AttributeResolver

--- flowsphere-common/src/main/java/com/flowsphere/common/request/AttributeResolver.java
+++ flowsphere-common/src/main/java/com/flowsphere/common/request/AttributeResolver.java
@@ -1,57 +1,17 @@
 package com.flowsphere.common.request;
 
-import com.flowsphere.common.constant.CommonConstant;
-import com.flowsphere.common.utils.StringUtils;
-
 public interface AttributeResolver {
 
     default String getTag(RequestResolver requestResolver) {
-        String tag = requestResolver.getHeader(CommonConstant.TAG);
-        if (StringUtils.isNotEmpty(tag)) {
-            return tag;
-        }
-        tag = requestResolver.getParameters(CommonConstant.TAG);
-        if (StringUtils.isNotEmpty(tag)) {
-            return tag;
-        }
-        tag = requestResolver.getCookie(CommonConstant.TAG);
-        if (StringUtils.isNotEmpty(tag)) {
-            return tag;
-        }
         return null;
     }
 
     default String getUserId(RequestResolver requestResolver) {
-        String userId = requestResolver.getHeader(CommonConstant.USER_ID);
-        if (StringUtils.isNotEmpty(userId)) {
-            return userId;
-        }
-        userId = requestResolver.getParameters(CommonConstant.USER_ID);
-        if (StringUtils.isNotEmpty(userId)) {
-            return userId;
-        }
-        userId = requestResolver.getCookie(CommonConstant.USER_ID);
-        if (StringUtils.isNotEmpty(userId)) {
-            return userId;
-        }
         return null;
     }
 
     default String getRegion(RequestResolver requestResolver) {
-        String region = requestResolver.getHeader(CommonConstant.REGION);
-        if (StringUtils.isNotEmpty(region)) {
-            return region;
-        }
-        region = requestResolver.getParameters(CommonConstant.REGION);
-        if (StringUtils.isNotEmpty(region)) {
-            return region;
-        }
-        region = requestResolver.getCookie(CommonConstant.REGION);
-        if (StringUtils.isNotEmpty(region)) {
-            return region;
-        }
         return null;
     }
-
 
 }
--- flowsphere-common/src/main/java/com/flowsphere/common/request/CookieResolver.java
+++ flowsphere-common/src/main/java/com/flowsphere/common/request/CookieResolver.java
@@ -1,6 +1,23 @@
 package com.flowsphere.common.request;
 
+import com.flowsphere.common.constant.CommonConstant;
+
 public interface CookieResolver extends AttributeResolver {
 
+
+    @Override
+    default String getTag(RequestResolver requestResolver) {
+        return requestResolver.getCookie(CommonConstant.TAG);
+    }
+
+    @Override
+    default String getUserId(RequestResolver requestResolver) {
+        return requestResolver.getCookie(CommonConstant.USER_ID);
+    }
+
+    @Override
+    default String getRegion(RequestResolver requestResolver) {
+        return requestResolver.getCookie(CommonConstant.REGION);
+    }
 
 }
